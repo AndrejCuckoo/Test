6150bc5d68806887dd6fbf42e24fe4c6881b9fe1
AndrejCuckoo <62504292+AndrejCuckoo@users.noreply.github.com>
2020-04-20 15:30:17 +0300

    update

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -41,38 +41,25 @@ vector<size_t> body(vector<double>& numbers,size_t bin_count){
     return bins;
 }
 
-void Hist(vector<size_t>& bins){
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
 
-    for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    svg_begin(400, 300);
+    svg_end();
 }
 
 int main() {
@@ -87,7 +74,7 @@ int main() {
     vector<size_t>bins = body(numbers,bin_count);
 
     // Вывод данных
-    Hist(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
