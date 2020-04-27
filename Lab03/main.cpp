3edb6123f2110881957e224bed6f9dd68b8060b1
AndrejCuckoo <62504292+AndrejCuckoo@users.noreply.github.com>
2020-04-27 15:04:06 +0300

    update

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -13,12 +13,11 @@ vector<double> input(size_t number_count){
 
 
 
-vector<size_t> body(vector<double>& numbers,size_t bin_count){
+vector<size_t> body(const vector<double>& numbers,size_t bin_count){
     // Обработка данных
     vector<size_t> bins(bin_count);
     double min;
     double max;
-    if(numbers.size()>0){
     find_minmax(numbers,min,max);
     for (double number : numbers) {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
@@ -27,13 +26,12 @@ vector<size_t> body(vector<double>& numbers,size_t bin_count){
         }
         bins[bin]++;
     }
-    }
     return bins;
 }
 
 void svg_text(double left,double baseline, string text)
 {
-    cout<< "<text x='" << left << "' y='" << baseline << "'>'"<< text<< "'</text>";
+    cout<< "<text x='" << left << "' y='" << baseline << "'>"<< text<< "</text>";
 }
 
 void
@@ -52,24 +50,28 @@ void svg_end() {
 }
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
 {
-    cout<< "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='aqua' fill='darkblue'/>";
+    cout<< "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='"<< stroke <<"' fill='"<<fill<<"'/>";
 }
 
-void show_histogram_svg(const vector<size_t>& bins) {
+void show_histogram_svg(const vector<size_t>& bins,double BIN_HEIGHT,double bin_count) {
     const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
+    const auto IMAGE_HEIGHT = 700;
     const auto TEXT_LEFT = 20;
     const auto TEXT_BASELINE = 20;
     const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+    const auto STROKE = "aqua";
+    const auto FILL = "darkblue";
+    if(bin_count*BIN_HEIGHT>IMAGE_HEIGHT){
+        BIN_HEIGHT =IMAGE_HEIGHT/bin_count;
+    }
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
     double top = 0;
     for (size_t bin : bins) {
         const double bin_width = BLOCK_WIDTH * bin;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,STROKE,FILL);
         top += BIN_HEIGHT;
     }
     svg_end();
@@ -83,10 +85,13 @@ int main() {
     vector <double> numbers = input(number_count);
     size_t bin_count;
     cin >> bin_count;
+    // Height
+    double bin_height;
+    cin >> bin_height;
     // Обработка данных
     const auto bins = body(numbers,bin_count);
     // Вывод данных
 
-    show_histogram_svg(bins);
+    show_histogram_svg(bins,bin_height,bin_count);
     return 0;
 }
